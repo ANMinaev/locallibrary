c3178c084412fff121542cd8e4430c66dc28e3fa
anmin <anminaev@gmail.com>
2020-11-10 20:15:26 +0300

    добвил что может вызвать не поятно

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -20,11 +20,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
-import catalog.views
+#import catalog.views
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
+ #urlpatterns = [
+    #Uncomment the next line to enable the admin:
+    # path('admin/', admin.site.urls),
+     #url(r'^$', catalog.views.index, name='index'),
+     #url(r'^home$', catalog.views.index, name='home'),
+ #]
 
 urlpatterns = [
-    #Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
-    url(r'^$', catalog.views.index, name='index'),
-    url(r'^home$', catalog.views.index, name='home'),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
