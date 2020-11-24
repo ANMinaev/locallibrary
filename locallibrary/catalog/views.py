5dc1d21dab6aa247f4a03deba50a570abf00e7d0
anmin <anminaev@gmail.com>
2020-11-24 23:22:08 +0300

    Первая домашняя страница

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
-def index(request):
+def index_old(request):
     now = datetime.now()
     html_content = "<html><head><title>Hello, Django</title></head><body>"
     html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
     html_content += "</body></html>"
     return HttpResponse( html_content)
+
+from catalog.models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
